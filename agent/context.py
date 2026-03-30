c4bef7356906a73862fdcd479e2696f59a594343
KevinYi <jiaoyi4@huawei.com>
2026-03-30 20:33:44 +0800

    feat: 上下文管理初始化文件

---
+++ agent/context.py
@@ -0,0 +1,16 @@
+TOOL_USE_ROUND_LIMIT = 3
+
+class MessageKeeper:
+    pass
+
+
+class ContextCompactor:
+    def __init__(self, tool_use_round_limit: int = TOOL_USE_ROUND_LIMIT):
+        self.tool_use_round_limit = tool_use_round_limit
+
+    def compact_tool_use():
+        pass
+
+
+class ContextManager:
+    pass
